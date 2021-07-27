0e0448397bbf5678150ff76854aa11bc9a252556
Morgan Roff <roffmorgan@gmail.com>
2021-07-27 21:57:46 +0200

    Fix timer

--- rp2040-hal/src/timer.rs
+++ rp2040-hal/src/timer.rs
@@ -1,15 +1,19 @@
 //! Timers
 // See [Chapter 4 Section 6](https://datasheets.raspberrypi.org/rp2040/rp2040_datasheet.pdf) for more details
 
-use embedded_time::fractional::Fraction;
+use crate::pac;
+use embedded_time::clock::Error;
+use embedded_time::duration::Microseconds;
+use embedded_time::fixed_point::FixedPoint;
+use embedded_time::fraction::Fraction;
 use embedded_time::{Clock, Instant};
 
 pub struct Timer {
-  device: p2040_pac::TIMER
+  device: pac::TIMER
 }
 
 impl Timer {
-    pub fn new(device: p2040_pac::TIMER) -> Timer {
+    pub fn new(device: pac::TIMER) -> Timer {
         Timer { device: device }
     }
 }
@@ -17,13 +21,24 @@ impl Timer {
 impl Clock for Timer {
     type T = u64;
 
-    const SCALING_FACTOR = Fraction::new(1, 1000);
+    // Clock operates on microsecond precision
+    const SCALING_FACTOR: Fraction = Microseconds::<u64>::SCALING_FACTOR;
 
     fn try_now(&self) -> Result<Instant<Self>, Error> {
         // Reading the lower 32 bits via `timelr` first latches the bits for `timehr` so that an
-        // accurate time value is read.
-        now: u64 = self.device.timelr.read() as u64;
-        now |= (self.device.timehr.read() as u64) << 32;
-        Ok(Instant::<Self>::new(now))
+        // accurate time value is read. However, this becomes unsafe if both cores are reading the
+        // timer concurrently (see Datasheet section 4.6.4.1). Therefore we perform a more
+        // complicated read over the latchless `aw*` registers instead.
+        let mut high: u32 = self.device.timerawh.read().bits();
+        let mut low: u32;
+        loop {
+            low = self.device.timerawl.read().bits();
+            let next_high: u32 = self.device.timerawh.read().bits();
+            if high == next_high {
+                break;
+            }
+            high = next_high;
+        }
+        Ok(Instant::<Self>::new((high as u64) << 32 | (low as u64)))
     }
 }
